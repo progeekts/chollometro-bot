35911f79f6c2bcf9c2f69eb92c58cc9b5175b050
ProGeekts <progeekts@gmail.com>
2026-03-27 10:45:55 +0100

    Decrease minimum temperature threshold from 350 to 300

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import time
 RSS_URL = "https://www.chollometro.com/rss/hot" 
 WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK") 
 CACHE_FILE = "vistos.txt"
-MIN_TEMP = 350
+MIN_TEMP = 300
 
 def get_temp(title):
     match = re.search(r"(\d+)[º°]", title)
